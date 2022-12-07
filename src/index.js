45458c35f27c472220dc36213dd8267c1e4a8a59
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2022-12-07 15:42:56 -0600

    Nested components and react tools

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-// stateless functional component
-// always must return JSX
-
-// JSX Rules
-// return single element
-// div / section/ article or fragment
-// use camelCase for html attributes (ex: onClick)
-// className instead of class
-// close every element
-// formatting
+// Nested Components, React Tools
 
 function Greeting(){
   return (
     <div>
-      <h1>Hello World</h1>
+      <Person />
+      <Message />
     </div>
   )
 }
 
-// DONT DO THIS MUCH MORE DIFFICULT
-// const Greeting = () => {
-//   return React.createElement('h1', {}, 'hello world')
-// 
+const Person = () => <h2>John Doe</h2>
+const Message = () => {
+  return <p>This is my message</p>
+}
 
 ReactDom.render(<Greeting />, document.getElementById('root'))
