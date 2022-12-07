21cced9509d0fcf004dec8070b08addfb972c9f9
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2022-12-07 15:37:26 -0600

    Detailed first component with comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+// stateless functional component
+// always must return JSX
+
+// JSX Rules
+// return single element
+// div / section/ article or fragment
+// use camelCase for html attributes (ex: onClick)
+// className instead of class
+// close every element
+// formatting
+
 function Greeting(){
-  return <h4>Hi I'm Jordan and this is my first REACT component</h4>
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
 }
+
+// DONT DO THIS MUCH MORE DIFFICULT
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'hello world')
+// 
 
 ReactDom.render(<Greeting />, document.getElementById('root'))
