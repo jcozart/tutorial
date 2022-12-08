0354b1816029edf052639fb2d1398db059da712a
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2022-12-08 09:13:47 -0600

    props destructuring

--- src/index.js
+++ src/index.js
@@ -26,13 +26,13 @@ function BookList(){
   )
 }
 
-const Book = (props) => {
-  console.log(props)
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props (this is the same as the destrucuring above)
   return ( 
     <article className='book'>
-      <img src={props.img} alt=''/>
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt=''/>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   )
 }
