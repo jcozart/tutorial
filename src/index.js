6c12236ef70a2e687f661b5f8568c8fd5caffc48
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2022-12-08 09:31:30 -0600

    Simple Lists

--- src/index.js
+++ src/index.js
@@ -4,41 +4,39 @@ import ReactDom from 'react-dom'
 import './index.css'
 
 
-// Setup variables
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/51uYhoxbcqL._AC_SY135_.jpg',
-  title: 'Network+',
-  author: 'Beau Dedmond'
+const books = [
+  {
+    img: 'https://m.media-amazon.com/images/I/51uYhoxbcqL._AC_SY135_.jpg',
+    title: 'Network+',
+    author: 'Beau Dedmond'
+  },
+  {
+    title: 'Security+',
+    author: 'Jordan Cozart',
+    img: 'https://m.media-amazon.com/images/I/41gsBLfjtkL._AC_SY135_.jpg',
+  }
+]
 
-}
-const secondBook = {
- title: 'Security+',
- author: 'Jordan Cozart',
- img: 'https://m.media-amazon.com/images/I/41gsBLfjtkL._AC_SY135_.jpg'
-}
-
+const names = ['john', 'peter', 'susan']
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>
+})
+console.log(newNames)
 function BookList(){
   return (
     <section className='booklist'>
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo
-          alias ea eaque nam dolorem a rerum 
-          corporis doloribus et dicta.
-        </p>
-      </Book>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
+      {newNames}
     </section>
   )
 }
 
 const Book = (props) => {
-  const { img, title, author, children } = props
+  const { img, title, author } = props
   return ( 
     <article className='book'>
       <img src={img} alt=''/>
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
