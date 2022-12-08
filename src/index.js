8dd06d49fc8b3e41087423a5099b188dbfa82676
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2022-12-08 09:45:22 -0600

    Proper lists and key addition

--- src/index.js
+++ src/index.js
@@ -6,32 +6,39 @@ import './index.css'
 
 const books = [
   {
+    id: 1,
     img: 'https://m.media-amazon.com/images/I/51uYhoxbcqL._AC_SY135_.jpg',
     title: 'Network+',
     author: 'Beau Dedmond'
   },
   {
+    id: 2,
     title: 'Security+',
     author: 'Jordan Cozart',
     img: 'https://m.media-amazon.com/images/I/41gsBLfjtkL._AC_SY135_.jpg',
+  },
+  {
+    id: 3,
+    title: 'Security+ Study Guide',
+    author: 'Brandon Shadrick',
+    img: 'https://m.media-amazon.com/images/I/5185gwoiZ3L._AC_SY135_.jpg'
   }
 ]
 
-const names = ['john', 'peter', 'susan']
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>
-})
-console.log(newNames)
 function BookList(){
   return (
     <section className='booklist'>
-      {newNames}
+      {books.map((book) => {
+        return (
+        <Book key={book.id} book={book}></Book>
+        )
+      })}
     </section>
   )
 }
 
 const Book = (props) => {
-  const { img, title, author } = props
+  const { img, title, author } = props.book
   return ( 
     <article className='book'>
       <img src={img} alt=''/>
