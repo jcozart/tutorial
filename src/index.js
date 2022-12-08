96331637203847bd4c6537e34d4b33757dee4a69
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2022-12-08 09:48:30 -0600

    Spread operator usage

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ function BookList(){
     <section className='booklist'>
       {books.map((book) => {
         return (
-        <Book key={book.id} book={book}></Book>
+        <Book key={book.id} {...book}></Book>
         )
       })}
     </section>
@@ -38,7 +38,7 @@ function BookList(){
 }
 
 const Book = (props) => {
-  const { img, title, author } = props.book
+  const { img, title, author } = props
   return ( 
     <article className='book'>
       <img src={img} alt=''/>
